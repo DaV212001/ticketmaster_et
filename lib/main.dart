5c66f8ac4900925039864bf56297ba6b26147169
JonathanZeru <jonathanzeru21@gmail.com>
2023-10-18 00:12:54 +0300

    Onboarding screen for first time users done.

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,8 @@ Future<void> appInit() async {
   print('COUNTRY CODE $countryCode');
   await settingsProvider.getCurrentThemeMode();
   print("1");
-  // settingsProvider.languageCode =
-  //     langCode + (countryCode.isNotEmpty ? '-' + countryCode : '');
+  settingsProvider.languageCode =
+      langCode + (countryCode.isNotEmpty ? '-' + countryCode : '');
   print("2");
   await settingsProvider.getLanguageCode();
   print("3");
@@ -42,8 +42,7 @@ void main() async {
     supportedLocales: Translation.all,
     path: 'assets/translations',
     fallbackLocale: const Locale('en'),
-    startLocale:Locale('en'),
-    // startLocale: Locale(langCode, countryCode),
+    startLocale: Locale(langCode, countryCode),
     child: TicketMasterET(
       settingsProvider: settingsProvider,
     ),
